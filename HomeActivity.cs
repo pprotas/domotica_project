246a9598fd42ac44c3bcff8a71370e01580e237a
Pawel Protas <pawprotas@gmail.com>
2018-06-13 13:21:31 +0200

    Added project files

---
+++ HomeActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+namespace HomeSafe9001
+{
+    [Activity(Label = "HomeActivity", Theme = "@style/AppTheme")]
+    public class HomeActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Create your application here  
+            SetContentView(Resource.Layout.Home);
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,82 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Java.Security;
+using Javax.Crypto;
+using Android.Hardware.Fingerprints;
+using Android.Support.V4.App;
+using Android;
+using System;
+using Android.Security.Keystore;
+
+namespace HomeSafe9001
+{
+    [Activity(Label = "FingerAuth", MainLauncher = true, Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        private KeyStore keyStore;
+        private Cipher cipher;
+        private string KEY_NAME = "Ahsan";
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource  
+            SetContentView(Resource.Layout.Main);
+            KeyguardManager keyguardManager = (KeyguardManager)GetSystemService(KeyguardService);
+            FingerprintManager fingerprintManager = (FingerprintManager)GetSystemService(FingerprintService);
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.UseFingerprint)
+                != (int)Android.Content.PM.Permission.Granted)
+                return;
+            if (!fingerprintManager.IsHardwareDetected)
+                Toast.MakeText(this, "FingerPrint authentication permission not enable", ToastLength.Short).Show();
+            else
+            {
+                if (!fingerprintManager.HasEnrolledFingerprints)
+                    Toast.MakeText(this, "Register at least one fingerprint in Settings", ToastLength.Short).Show();
+                else
+                {
+                    if (!keyguardManager.IsKeyguardSecure)
+                        Toast.MakeText(this, "Lock screen security not enable in Settings", ToastLength.Short).Show();
+                    else
+                        GenKey();
+                    if (CipherInit())
+                    {
+                        FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                        FingerprintHandler handler = new FingerprintHandler(this);
+                        handler.StartAuthentication(fingerprintManager, cryptoObject);
+                    }
+                }
+            }
+        }
+        private bool CipherInit()
+        {
+            try
+            {
+                cipher = Cipher.GetInstance(KeyProperties.KeyAlgorithmAes
+                    + "/"
+                    + KeyProperties.BlockModeCbc
+                    + "/"
+                    + KeyProperties.EncryptionPaddingPkcs7);
+                keyStore.Load(null);
+                IKey key = (IKey)keyStore.GetKey(KEY_NAME, null);
+                cipher.Init(CipherMode.EncryptMode, key);
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+        private void GenKey()
+        {
+            keyStore = KeyStore.GetInstance("AndroidKeyStore");
+            KeyGenerator keyGenerator = null;
+            keyGenerator = KeyGenerator.GetInstance(KeyProperties.KeyAlgorithmAes, "AndroidKeyStore");
+            keyStore.Load(null);
+            keyGenerator.Init(new KeyGenParameterSpec.Builder(KEY_NAME, KeyStorePurpose.Encrypt | KeyStorePurpose.Decrypt)
+                .SetBlockModes(KeyProperties.BlockModeCbc)
+                .SetUserAuthenticationRequired(true)
+                .SetEncryptionPaddings(KeyProperties
+                .EncryptionPaddingPkcs7).Build());
+            keyGenerator.GenerateKey();
+        }
+    }
+}
