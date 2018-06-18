9929ec1e78e4677ec3cf50bb7e798873d900aa65
Pawel Protas <pawprotas@gmail.com>
2018-06-18 12:07:24 +0200

    Added Arduino server file

---
+++ Xamarin/FingerprintHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android;
+using Android.Content;
+using Android.Hardware.Fingerprints;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Widget;
+
+namespace HomeSafe9001
+{
+    internal class FingerprintHandler : FingerprintManager.AuthenticationCallback
+    {
+        private Context mainActivity;
+        public FingerprintHandler(Context mainActivity)
+        {
+            this.mainActivity = mainActivity;
+        }
+        internal void StartAuthentication(FingerprintManager fingerprintManager, FingerprintManager.CryptoObject cryptoObject)
+        {
+            CancellationSignal cancellationSignal = new CancellationSignal();
+            if (ActivityCompat.CheckSelfPermission(mainActivity, Manifest.Permission.UseFingerprint)
+                != (int)Android.Content.PM.Permission.Granted)
+                return;
+            fingerprintManager.Authenticate(cryptoObject, cancellationSignal, 0, this, null);
+        }
+        public override void OnAuthenticationFailed()
+        {
+            Toast.MakeText(mainActivity, "Fingerprint Authentication failed!", ToastLength.Long).Show();
+        }
+        public override void OnAuthenticationSucceeded(FingerprintManager.AuthenticationResult result)
+        {
+            mainActivity.StartActivity(new Intent(mainActivity, typeof(HomeActivity)));
+        }
+    }
+}
